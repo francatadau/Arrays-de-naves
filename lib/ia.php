9ec0eabfeea5d86cc6399dfc96707ecf92bde9c8
forpi <francisco.ortola.piles@gmail.com>
2016-10-19 11:17:40 +0200

    Sin limites

---
+++ lib/ia.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ *
+ */
+class ia
+{
+  //Declaramos las propiedades
+  public $fila = '3';
+  public $columna = '1';
+
+  //Declaramos el metodo
+  public function getFila(){
+    return $this->fila;
+  }
+  public function getColumna(){
+    return $this->columna;
+  }
+  // Setters
+  public function setFila($fil){
+    if ($fil>3){
+    $this->fila=3;
+  } elseif ($fil<0){
+    $this->fila=0;
+  }else {
+    $this->fila=$fil;
+  }
+}
+  public function setColumna($col){
+    if ($col>3){
+    $this->columna=3;
+  } elseif ($col<0){
+    $this->columna=0;
+  } else {
+    $this->columna=$col;
+  }
+
+  }
+  // Aleatorio
+  public function RandomPos(){
+
+  }
+}
+?>
